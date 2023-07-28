a93e9a787614a07a2f3dad5c15fde70c2a3477e0
Ming0099 <130200978+Ming0099@users.noreply.github.com>
2023-07-28 21:36:42 +0900

    동글이 오류 수정

--- Assets/Scene/Game/Symmetry/SdiffText.cs
+++ Assets/Scene/Game/Symmetry/SdiffText.cs
@@ -6,13 +6,13 @@ using UnityEngine.UI;
 
 public class SdiffText : MonoBehaviour
 {
-    public Text STextDiff;
+    public GameObject TextObj;
     public GameObject CanvasTextS;
     private bool clickEvent = true;
 
     void Start()
     {
-        STextDiff.text = "level of difficulty \n\n" + "Symmetry : " + LoginController.myDiffData.SymmetryGameDifficulty.ToString();
+        TextObj.GetComponent<Text>().text = "level of difficulty \n\n" + "Symmetry : " + LoginController.myDiffData.SymmetryGameDifficulty.ToString();
     }
 
     void OnMouseDown()
